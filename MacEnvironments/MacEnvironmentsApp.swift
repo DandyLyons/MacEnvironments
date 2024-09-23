728acf37452a81756bf0da5641cb430252032422
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2024-09-23 12:47:41 -0600

    Add MacCatalyst scheme

--- MacEnvironments/MacEnvironmentsApp.swift
+++ MacEnvironments/MacEnvironmentsApp.swift
@@ -12,73 +12,74 @@ import SwiftUI
 struct MacEnvironmentsApp: App {
     var body: some Scene {
         WindowGroup {
-            Text("Hello World")
+            Text("MacEnvironmentsApp")
 #if os(macOS)
-            .onAppear {
-              reportIssue("#if os(macOS)")
-              if ProcessInfo.processInfo.isMacCatalystApp {
-                reportIssue("ProcessInfo.processInfo.isMacCatalystApp")
-              }
-              if ProcessInfo.processInfo.isiOSAppOnMac {
-                reportIssue("ProcessInfo.processInfo.isiOSAppOnMac")
-              }
-              
-              // 🔴 UIDevice not available here
-//              switch UIDevice.current.userInterfaceIdiom {
-//                case .pad, .mac, .phone: reportIssue()
-//                default: reportIssue()
-//              }
-            }
+                .onAppear {
+                    reportIssue("#if os(macOS)")
+                    if ProcessInfo.processInfo.isMacCatalystApp {
+                        reportIssue("ProcessInfo.processInfo.isMacCatalystApp")
+                    }
+                    if ProcessInfo.processInfo.isiOSAppOnMac {
+                        reportIssue("ProcessInfo.processInfo.isiOSAppOnMac")
+                    }
+                    
+                    // 🔴 UIDevice not available here
+                    //              switch UIDevice.current.userInterfaceIdiom {
+                    //                case .pad, .mac, .phone: reportIssue()
+                    //                default: reportIssue()
+                    //              }
+                    
+                }
 #elseif os(iOS)
-            .onAppear {
-              reportIssue("#if os(iOS)")
-              if ProcessInfo.processInfo.isMacCatalystApp {
-                reportIssue("ProcessInfo.processInfo.isMacCatalystApp")
-              }
-              if ProcessInfo.processInfo.isiOSAppOnMac {
-                reportIssue("ProcessInfo.processInfo.isiOSAppOnMac")
-              }
-              
-              switch UIDevice.current.userInterfaceIdiom {
-                  // by default Catalyst apps will use .pad
-                case .pad: reportIssue("UIDevice.current.userInterfaceIdiom.pad")
-                  // To use .mac, in your Catalyst target, go to Project > [Catalyst Target] > General > Deployment Info > Mac Catalyst Interface and set it to "Optimize for Mac"
-                case .mac: reportIssue("UIDevice.current.userInterfaceIdiom.mac")
-                default: reportIssue()
-              }
-            }
+                .onAppear {
+                    reportIssue("#if os(iOS)")
+                    if ProcessInfo.processInfo.isMacCatalystApp {
+                        reportIssue("ProcessInfo.processInfo.isMacCatalystApp")
+                    }
+                    if ProcessInfo.processInfo.isiOSAppOnMac {
+                        reportIssue("ProcessInfo.processInfo.isiOSAppOnMac")
+                    }
+                    
+                    switch UIDevice.current.userInterfaceIdiom {
+                            // by default Catalyst apps will use .pad
+                        case .pad: reportIssue("UIDevice.current.userInterfaceIdiom.pad")
+                            // To use .mac, in your Catalyst target, go to Project > [Catalyst Target] > General > Deployment Info > Mac Catalyst Interface and set it to "Optimize for Mac"
+                        case .mac: reportIssue("UIDevice.current.userInterfaceIdiom.mac")
+                        default: reportIssue()
+                    }
+                }
 #endif
-          
+            
 #if targetEnvironment(macCatalyst)
-            .onAppear {
-              reportIssue("#if targetEnvironment(macCatalyst)")
-            }
+                .onAppear {
+                    reportIssue("#if targetEnvironment(macCatalyst)")
+                }
 #endif
-          
+            
 #if targetEnvironment(simulator)
-            .onAppear{
-              reportIssue("targetEnvironment(simulator)")
-            }
+                .onAppear{
+                    reportIssue("targetEnvironment(simulator)")
+                }
 #endif
-          
+            
 #if canImport(UIKit)
-            .onAppear { reportIssue("#if canImport(UIKit)") }
+                .onAppear { reportIssue("#if canImport(UIKit)") }
 #endif
-          
+            
 #if canImport(AppKit)
-            .onAppear { reportIssue("#if canImport(AppKit)") }
+                .onAppear { reportIssue("#if canImport(AppKit)") }
 #endif
-          
+            
 #if arch(x86_64)
-            .onAppear{
-              reportIssue("arch(x86_64)")
-            }
+                .onAppear{
+                    reportIssue("arch(x86_64)")
+                }
 #endif
-          
+            
 #if arch(arm64)
-            .onAppear {
-              reportIssue("arch(arm64)")
-            }
+                .onAppear {
+                    reportIssue("arch(arm64)")
+                }
 #endif
         }
     }
